be5ab4a68076d8800df7d5dad8f0a18d572f051f
Benjamin Löffel <benjaminloeffel@gmail.com>
2020-03-09 16:46:49 +0100

    Display user name, email, image, and dob in the card component

--- components/Card.js
+++ components/Card.js
@@ -1,16 +1,25 @@
 import Link from "next/link";
 import { Card as StyledCard, Image, Text } from "theme-ui";
 
-const Card = ({ picture, login, name }) => {
+const Card = ({ picture, login, name, dob, email }) => {
+  const date = new Date(dob.date.toString());
+  const formattedDate = date.toLocaleString("en-GB", {
+    day: "numeric",
+    month: "short",
+    year: "numeric"
+  });
+
   return (
     <StyledCard key={login.uuid}>
-      <Image src={picture.medium} />
+      <Image src={picture.large} variant="avatar" />
       <Text>
         <Link href={`/users/${login.uuid}`}>
           <a>
-            {name.first} {name.last}
+            {name.title}. {name.first} {name.last}
           </a>
         </Link>
+        <p>{formattedDate}</p>
+        <p>{email}</p>
       </Text>
     </StyledCard>
   );
