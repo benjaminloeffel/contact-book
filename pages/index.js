dbd8d941e265148b05ae52cd5099ad761a970e8d
Benjamin Löffel <benjaminloeffel@gmail.com>
2020-03-09 20:22:54 +0100

    Changed parsed user by api to 10

--- pages/index.js
+++ pages/index.js
@@ -44,7 +44,7 @@ const Home = ({ users }) => {
 };
 
 Home.getInitialProps = async () => {
-  const response = await fetch("https://randomuser.me/api/?results=5");
+  const response = await fetch("https://randomuser.me/api/?results=10");
   const data = await response.json();
 
   return {
