3e26b5521871eac2dc90967e524832a9f2aa5d53
Benjamin Löffel <benjaminloeffel@gmail.com>
2020-03-10 11:26:57 +0100

    Add title and meta description to document head

--- components/layout.js
+++ components/layout.js
@@ -1,7 +1,16 @@
+import Head from "next/head";
 import Header from "./Header";
 
 const Layout = ({ children }) => (
   <div>
+    <Head>
+      <title>My Contacts</title>
+      <meta
+        name="viewport"
+        content="initial-scale=1.0, width=device-width"
+        key="viewport"
+      />
+    </Head>
     <Header />
     <main>{children}</main>
   </div>
