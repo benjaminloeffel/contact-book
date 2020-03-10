195a0b63b4300dfbefac5bb98d0b2f42121472c1
Benjamin Löffel <benjaminloeffel@gmail.com>
2020-03-10 18:29:26 +0100

    Add id to input field to increase accessibility score for the input label

--- components/SearchForm.js
+++ components/SearchForm.js
@@ -5,6 +5,7 @@ const SearchForm = ({ onSearchChange, searchTerm }) => (
   <Box as="form" onSubmit={e => e.preventDefault()}>
     <Label htmlFor="search">Search Contacts</Label>
     <Input
+      id="search"
       type="text"
       placeholder="Enter the name of the contact"
       name="search"
